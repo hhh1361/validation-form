5b2a94f26316b48084a25c52636b95980fd24cc8
hhh1361 <nikita.turlyko@gmail.com>
2019-12-04 20:40:34 +0300

    Updates

---
+++ precache-manifest.0ddb9ce310cd9963721498a997743bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800f9592d33491802e326fc80ac77afd",
+    "url": "/validation-form/index.html"
+  },
+  {
+    "revision": "6d28b56fd757606615d6",
+    "url": "/validation-form/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7c512138e29c08802b88",
+    "url": "/validation-form/static/css/main.30315265.chunk.css"
+  },
+  {
+    "revision": "6d28b56fd757606615d6",
+    "url": "/validation-form/static/js/2.6010ebfe.chunk.js"
+  },
+  {
+    "revision": "7c512138e29c08802b88",
+    "url": "/validation-form/static/js/main.6c873c3b.chunk.js"
+  },
+  {
+    "revision": "f0cdc7a53ff2db772406",
+    "url": "/validation-form/static/js/runtime-main.341dc8fa.js"
+  }
+]);
